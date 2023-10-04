63c31a9739e63f6d2784c81838d5d4963da9568c
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-10-04 20:50:27 +0300

    deleted accidental repeated code

--- components/icons/outline-icons/ArrowIcon.tsx
+++ components/icons/outline-icons/ArrowIcon.tsx
@@ -39,18 +39,5 @@ ArrowIcon.Right = function ArrowRightIcon() {
     </ArrowIcon>
   );
 };
-ArrowIcon.Right = function ArrowRightIcon() {
-  return (
-    <ArrowIcon>
-      <path
-        d="M4 10H16M16 10L11.3333 5M16 10L11.3333 15"
-        className="stroke-[#3F4354] dark:stroke-[#F7F7F7]"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </ArrowIcon>
-  );
-};
 
 export default ArrowIcon;
