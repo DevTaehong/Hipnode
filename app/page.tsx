1f5e19d284a28d8d3938014a5b4a5ce3cd7a00b3
Tye Stanley <tyestanley090@gmail.com>
2023-10-03 21:14:24 -0700

    feat: added all the fill icons and exported them from index file

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,70 @@
-import { HomeIcon } from "@/components/icons/fill-icons";
+import {
+  HomeIcon,
+  CommentIcon,
+  RocketIcon,
+  CalendarIcon,
+  MessageIcon,
+  ProfileIcon,
+  ReplyIcon,
+  NotificationIcon,
+  GroupIcon,
+  SunIcon,
+  ShareIcon,
+  GoogleIcon,
+  FacebookIcon,
+  TwitterIcon,
+  TroubleIcon,
+  SendIcon,
+  SettingsIcon,
+  LeaveIcon,
+  MoonIcon,
+  PostIcon,
+  PodcastsIcon,
+  FireIcon,
+  HeartIcon,
+  GrowingIcon,
+  ReportIcon,
+  FeedbackIcon,
+  InboxIcon,
+  InterviewsIcon,
+  BusinessIcon,
+  FollowIcon,
+  MenuIcon
+} from "@/components/icons/fill-icons";
 export default function Home() {
   return (
     <main className="flex gap-1">
       <HomeIcon />
+      <CommentIcon />
+      <RocketIcon />
+      <CalendarIcon />
+      <MessageIcon />
+      <ProfileIcon />
+      <ReplyIcon />
+      <NotificationIcon />
+      <GroupIcon />
+      <SunIcon />
+      <ShareIcon />
+      <GoogleIcon />
+      <FacebookIcon />
+      <TwitterIcon />
+      <TroubleIcon />
+      <SendIcon />
+      <SettingsIcon />
+      <LeaveIcon />
+      <MoonIcon />
+      <PostIcon />
+      <PodcastsIcon />
+      <FireIcon />
+      <HeartIcon />
+      <GrowingIcon />
+      <ReportIcon />
+      <FeedbackIcon />
+      <InboxIcon />
+      <InterviewsIcon />
+      <BusinessIcon />
+      <FollowIcon />
+      <MenuIcon />
     </main>
   );
 }
---
+++ components/icons/fill-icons/MenuIcon.tsx
@@ -0,0 +1,16 @@
+export default function MenuIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <path
+        d="M0 2V4.99996H20V2H0ZM0 8.50002V11.5H20V8.50002H0ZM0 15V18H20V15H0Z"
+        fill="#3F4354"
+      />
+    </svg>
+  );
+}
