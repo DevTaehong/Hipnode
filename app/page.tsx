db5792daba140a0a52181da0eb99abdcc2f9144a
Tye Stanley <tyestanley090@gmail.com>
2023-10-05 11:21:36 -0700

    fix: removed testing code from main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,3 @@
-import FillIcon from "@/components/icons/fill-icons";
 export default function Home() {
-  return (
-    <main className="">
-      <FillIcon.Business />
-      <FillIcon.Notification notifcation />
-      <FillIcon.Notification />
-      <FillIcon.Group notifcation />
-      <FillIcon.Group />
-      <FillIcon.Follow />
-
-      <FillIcon.Home />
-    </main>
-  );
+  return <main className=""></main>;
 }
