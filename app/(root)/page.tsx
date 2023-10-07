b9e394bc23dc513fdc23eae04a86fe458cfd2f54
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-10-07 04:53:01 +0300

    started custom sign in and out buttons

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, SignOutButton, SignInButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
+      <UserButton
+        afterSignOutUrl="/"
+        appearance={{
+          elements: {
+            userButtonPopoverCard: "dark:bg-dark-dark2",
+            userPreviewMainIdentifier: "dark:text-white",
+            userPreviewSecondaryIdentifier: "dark:text-lightBackground-5",
+            userButtonPopoverActionButtonText: "dark:text-white",
+            userButtonPopoverActionButtonIcon: "dark:invert",
+            userButtonPopoverFooter: "dark:invert",
+            userPreviewAvatarContainer: "dark:grayscale-[60%]",
+          },
+        }}
+      />
+      <SignInButton>Sign In</SignInButton>
+      <SignOutButton />
     </div>
   );
 }
