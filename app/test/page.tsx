e47d9cbb6402a3be9f66de2743712625f0980f61
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-10-10 00:26:03 +0300

    getting started

---
+++ app/test/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MeetupsCard from "@/components/MeetupsCard";
+
+const page = () => {
+  return (
+    <div className="flex">
+      <div className="flex">
+        <MeetupsCard />
+      </div>
+    </div>
+  );
+};
+
+export default page;
---
+++ components/MeetupsCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MeetupsCard = () => {
+  return (
+    <div>
+      <div>content</div>
+    </div>
+  );
+};
+
+export default MeetupsCard;
