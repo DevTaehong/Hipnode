236536d67353551603564f00111039e6e665285f
AlexDjangoX <alexmonk17@gmail.com>
2023-10-10 11:33:45 +0200

    extracted ThemeToggle

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,63 +1,14 @@
 'use client';
 
-import React from 'react';
-import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-
-import { useTheme } from '@/context/ThemeProvider';
 import { HipnodeIcon } from './icons/outline-icons';
-import ActionButtons from './action-buttons/ActionButtons';
-import CustomButton from './CustomButton';
+import ThemeToggle from '@/context/ThemeToggle';
 
 const Nav = () => {
-  const { mode, setMode } = useTheme();
-  const pathname = usePathname();
-
-  const toggleTheme = () => {
-    const newMode = mode === 'light' ? 'dark' : 'light';
-    setMode(newMode);
-    localStorage.setItem('theme', newMode);
-  };
-
   return (
-    <>
-      <div className="flex  h-14 w-full items-center justify-between bg-light px-10 dark:bg-dark-2">
-        <HipnodeIcon />
-        <button
-          className="mr-6 flex h-6 w-6 items-center justify-center"
-          onClick={toggleTheme}
-          aria-label="Toggle Theme"
-        >
-          {mode === 'light' ? (
-            <Image
-              src="/assets/icons/sun.svg"
-              alt="sun"
-              width={20}
-              height={20}
-            />
-          ) : (
-            <Image
-              src="/assets/icons/moon.svg"
-              alt="moon"
-              width={20}
-              height={20}
-            />
-          )}
-        </button>
-      </div>
-      <ActionButtons currentPath={pathname} />
-      <div className="flex justify-center gap-4">
-        <CustomButton
-          className="bg-sc-6 text-sc-2 dark:bg-dark-4 dark:text-sc-6"
-          label="Advertising"
-        />
-        <CustomButton
-          className="bg-dark-4 text-sc-6 dark:bg-sc-6 dark:text-sc-2"
-          label="Advertising"
-        />
-        <CustomButton label="Advertising" />
-      </div>
-    </>
+    <div className="flex h-14 w-full items-center justify-between bg-light px-10 dark:bg-dark-2">
+      <HipnodeIcon />
+      <ThemeToggle />
+    </div>
   );
 };
 
