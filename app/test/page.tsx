b6efce1e5c942072b726d5ddcdca4b379d7b299d
DevTaehong <minth1123@icloud.com>
2023-10-12 18:01:43 -0300

    merge: merge the main branch

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import MessagesPopover from "@/components/MessagesPopover";
+
+const page = () => (
+  <div className="h-screen bg-light-2 p-4 dark:bg-dark-2">
+    <MessagesPopover />
+  </div>
+);
+
+export default page;
