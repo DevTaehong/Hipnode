ce26a1192f30736b7fa33f771e57daf3d2bda9b3
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-10-12 11:44:00 +0300

    changes to webhooks

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -10,7 +10,7 @@ import { redirect } from "next/navigation";
 const Page = async () => {
   const user = await currentUser();
   if (!user) {
-    redirect("/sign-up");
+    redirect("/");
   }
   const { id: userClerkId } = user;
 
