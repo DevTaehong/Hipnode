d14e909ddfde24f7e089cc8f4d1c2275d32d0aeb
AlexDjangoX <alexmonk17@gmail.com>
2023-10-13 14:25:30 +0200

    implement PrismaClient singleton pattern

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prismaSingleton = () => new PrismaClient();
+
+type PrismaClientSingleton = ReturnType<typeof prismaSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
