8aefd2ba34fb12e3b9598d22a793adda07d58fb6
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-10-16 15:55:54 +0300

    added await

--- app/api/webhooks/user/route.ts
+++ app/api/webhooks/user/route.ts
@@ -84,7 +84,7 @@ async function handler(request: Request) {
 
     const emailAddress = email_addresses[0].email_address;
 
-    const user = createUser({
+    const user = await createUser({
       clerkId: id,
       name: `${first_name} ${last_name}`,
       username,
