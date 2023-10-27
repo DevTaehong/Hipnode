4e7580965d616dab4a49b7b27999f8309806d66e
Tye Stanley <tyestanley090@gmail.com>
2023-10-27 10:53:12 -0700

    feat: setup file structure for profile page

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="">
+      {/* Profile Modal */}
+      <section className=""></section>
+
+      {/* Profile Filter & Content Cards */}
+      <section className=""></section>
+
+      {/* HostMeetup Card & Performance Card */}
+      <section className=""></section>
+    </div>
+  );
+};
+
+export default Page;
