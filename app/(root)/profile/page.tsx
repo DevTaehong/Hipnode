e33192e9a90c7f119e829e52194c9f4521614124
Tye Stanley <tyestanley090@gmail.com>
2023-10-27 21:13:25 -0700

    feat: created ProfileInfoCard

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,16 +1,122 @@
-import React from "react";
+import ContentCard from "@/components/profile/ContentCard";
+import HostMeetupCard from "@/components/profile/HostMeetupCard";
+import PerformanceCard from "@/components/profile/PerformanceCard";
+import ProfileFilter from "@/components/profile/ProfileFilter";
+import ProfileInfo from "@/components/profile/ProfileInfo";
+
+// TODO: Replace this with data from the API
+// ! Keeping it here for now to make it easier to work on the UI and implement database intergration later
+const profileData = {
+  src: "/emoji_2.png",
+  name: "AR. Jakir",
+  title: "User Interface Designer",
+  points: "880",
+  description:
+    "Hey there... I'm AR Jakir! I'm here to learn from and support the other members of this community!",
+  website: "https://google.com",
+  joinedAt: "joined 2 years ago",
+  followers: [
+    {
+      name: "Tye1",
+      src: "/emoji_2.png",
+      link: "/profile-page",
+    },
+    {
+      name: "Tye2",
+      src: "/emoji_2.png",
+      link: "/profile-page",
+    },
+  ],
+  following: [
+    {
+      id: "1",
+      name: "Tye1",
+      src: "/emoji_2.png",
+      link: "/profile-page1",
+    },
+    {
+      id: "2",
+      name: "Tye2",
+      src: "/emoji_2.png",
+      link: "/profile-page2",
+    },
+    {
+      id: "3",
+      name: "Tye3",
+      src: "/emoji_2.png",
+      link: "/profile-page3",
+    },
+    {
+      id: "4",
+      name: "Tye4",
+      src: "/emoji_2.png",
+      link: "/profile-page4",
+    },
+    {
+      id: "5",
+      name: "Tye5",
+      src: "/emoji_2.png",
+      link: "/profile-page5",
+    },
+    {
+      id: "6",
+      name: "Tye6",
+      src: "/emoji_2.png",
+      link: "/profile-page6",
+    },
+    {
+      id: "7",
+      name: "Tye7",
+      src: "/emoji_2.png",
+      link: "/profile-page7",
+    },
+  ],
+  socials: [
+    {
+      name: "Twitter",
+      link: "https://twitter.com/",
+    },
+    {
+      name: "Facebook",
+      link: "https://facebook.com/",
+    },
+    {
+      name: "Instagram",
+      link: "https://instagram.com/",
+    },
+  ],
+};
 
 const Page = () => {
   return (
-    <div className="">
+    <div className="flex flex-col justify-center gap-5 bg-light-2 p-5 dark:bg-dark-2 md:flex-row">
       {/* Profile Modal */}
-      <section className=""></section>
+      <section className="">
+        <ProfileInfo
+          src={profileData.src}
+          name={profileData.name}
+          title={profileData.title}
+          followers={profileData.followers.length}
+          points={profileData.points}
+          following={profileData.following}
+          description={profileData.description}
+          website={profileData.website}
+          socials={profileData.socials}
+          joinedAt={profileData.joinedAt}
+        />
+      </section>
 
       {/* Profile Filter & Content Cards */}
-      <section className=""></section>
+      <section className="">
+        <ProfileFilter />
+        <ContentCard />
+      </section>
 
       {/* HostMeetup Card & Performance Card */}
-      <section className=""></section>
+      <section className="">
+        <HostMeetupCard />
+        <PerformanceCard />
+      </section>
     </div>
   );
 };
---
+++ components/profile/ProfileInfo.tsx
@@ -0,0 +1,137 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import ProfileBtns from "./ProfileBtns";
+import OutlineIcon from "../icons/outline-icons";
+
+// ! Keeping props here since they will be changing when we implement database intergration
+interface ProfileInfoProps {
+  src: string;
+  name: string;
+  title: string;
+  followers: number;
+  points: string;
+  following: { id: string; name: string; src: string; link: string }[];
+  description: string;
+  website?: string;
+  socials: { name: string; link: string }[];
+  joinedAt: string;
+}
+
+const ProfileModal = ({
+  src,
+  name,
+  title,
+  followers,
+  points,
+  following,
+  description,
+  website,
+  socials,
+  joinedAt,
+}: ProfileInfoProps) => {
+  return (
+    <div className="relative flex flex-col items-center rounded-2xl bg-white p-5 dark:bg-dark-3">
+      {/* Orange background image with absolute positioning */}
+      <div className="absolute left-0 top-0 h-[6.50rem] w-full rounded-t-2xl bg-profile-modal bg-cover bg-center bg-no-repeat" />
+
+      {/* Profile Image that changes the border color for light/dark mode */}
+      <Image
+        src={src}
+        alt="profile"
+        width={130}
+        height={130}
+        className="relative mx-5 mt-5 rounded-full border-4 border-white bg-yellow-30 dark:border-dark-3"
+      />
+
+      <h3 className="mt-2.5 text-[1.625rem] font-semibold leading-[2.375rem] text-sc-1 dark:text-light">
+        {name}
+      </h3>
+
+      <p className="text-[1rem] leading-[1.5rem] text-sc-2 dark:text-sc-3">
+        {title}
+      </p>
+
+      {/* Made the Follow & Message buttons into their own components since both will use onClick which requires "use client" */}
+      <ProfileBtns />
+
+      <p className="mt-5 text-[0.875rem] font-semibold leading-[1.375rem] text-sc-2 dark:text-sc-6">
+        {followers} Followers • {points} Points
+      </p>
+
+      <p className="mt-5 text-[0.875rem] font-semibold leading-[1.375rem] text-sc-2 dark:text-sc-6">
+        Following {following.length}
+      </p>
+
+      {/* Gets all followers then slice all but 6 to run a map on that then returns clickable images of the users profiles */}
+      <div className="mt-4 flex flex-wrap justify-center gap-2.5">
+        {following.slice(0, 6).map((profile) => (
+          <Link key={profile.id} href={profile.link}>
+            <Image
+              src={profile.src}
+              alt="profile"
+              width={30}
+              height={30}
+              className="rounded-full bg-sc-6 dark:border-dark-3"
+            />
+          </Link>
+        ))}
+
+        {/* If user has more than 6 followings then this shows how many more they are following minus 6 that is already showing */}
+        {following.length > 6 && (
+          <Link
+            // TODO: Decide on where to take the user when they click on the + button
+            href="/"
+            className="flex h-[30px] w-[30px] items-center justify-center rounded-full bg-sc-6"
+          >
+            <p className="text-[0.875rem] font-semibold leading-[1.375rem] text-sc-2">
+              {following.length - 6}+
+            </p>
+          </Link>
+        )}
+      </div>
+
+      <p className="mt-5 w-[200px] text-center text-[0.875rem] font-semibold leading-[1.375rem] text-sc-3">
+        {description}
+      </p>
+
+      <div className="flex flex-wrap justify-center gap-5 md:flex-col">
+        {/* Website Link */}
+        {website && (
+          <Link
+            href={website}
+            className="mt-5 flex items-center gap-2 text-sc-2 dark:text-sc-6"
+          >
+            <OutlineIcon.Web className="fill-sc-2 dark:fill-light-2" />
+            <p className="w-[130px] truncate">{website}</p>
+          </Link>
+        )}
+
+        {/* Social Icons */}
+        <div className="mt-5 flex justify-center gap-5">
+          {socials.map((social) => {
+            const SocialIcon =
+              OutlineIcon[social.name as keyof typeof OutlineIcon];
+
+            return (
+              <Link key={social.name} href={social.link}>
+                <SocialIcon className="fill-sc-4 dark:fill-sc-6" />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* div line that seperates socials from when the user joined */}
+      <div className="mt-5 h-[1px] w-full bg-light-2 dark:bg-sc-3 md:mt-7" />
+
+      {/* Shows when the account joined hipnode */}
+      {/* TODO: create a function that returns the proper date format */}
+      <p className="mt-5 text-[0.875rem] font-semibold leading-[1.375rem] text-sc-3 dark:text-sc-6 md:mt-7">
+        {joinedAt}
+      </p>
+    </div>
+  );
+};
+
+export default ProfileModal;
