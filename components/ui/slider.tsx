242c6b1d9be7da6a022a258840d321989d78f25e
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-10-29 02:22:27 +0300

    fixed outline colour

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -21,7 +21,7 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Track className="relative h-40 w-2 grow overflow-hidden rounded-full bg-sc-5 dark:bg-dark-4">
       <SliderPrimitive.Range className="absolute h-full" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full bg-red-60 hover:bg-red-80 disabled:pointer-events-none disabled:opacity-50 " />
+    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full bg-red-60 hover:bg-red-80 focus:outline-none focus:ring-0 disabled:pointer-events-none disabled:opacity-50 " />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
