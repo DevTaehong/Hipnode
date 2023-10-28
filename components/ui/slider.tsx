aef06279a475d1d0b1986657aeb96f62b5c1e374
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-10-29 02:15:18 +0300

    changed slider icon colour

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -21,7 +21,7 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Track className="relative h-40 w-2 grow overflow-hidden rounded-full bg-sc-5 dark:bg-dark-4">
       <SliderPrimitive.Range className="absolute h-full" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full bg-red-60 transition duration-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 " />
+    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full bg-red-60 hover:bg-red-80 disabled:pointer-events-none disabled:opacity-50 " />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
