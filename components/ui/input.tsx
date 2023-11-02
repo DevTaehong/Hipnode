709025adae7181eb84d4899938516065811af54f
AlexDjangoX <alexmonk17@gmail.com>
2023-11-02 18:28:50 +0100

    made preview changes

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <input
         type={type}
-        className={cn("flex  w-full", className)}
+        className={cn("flex w-full", className)}
         ref={ref}
         {...props}
       />
