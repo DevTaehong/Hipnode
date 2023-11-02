dae301530730fa444de60878d1655db8f7d974dc
Tye Stanley <tyestanley090@gmail.com>
2023-11-02 12:10:24 -0700

    fix: removed return statement in MoreVerticalIcon component

--- components/icons/fill-icons/MoreVerticalIcon.tsx
+++ components/icons/fill-icons/MoreVerticalIcon.tsx
@@ -1,13 +1,9 @@
-import React from "react";
-
-const MoreVerticalIcon = () => {
-  return (
-    <>
-      <circle cx="10" cy="17.5" r="2.5" transform="rotate(-90 10 17.5)" />
-      <circle cx="10" cy="10" r="2.5" transform="rotate(-90 10 10)" />
-      <circle cx="10" cy="2.5" r="2.5" transform="rotate(-90 10 2.5)" />
-    </>
-  );
-};
+const MoreVerticalIcon = () => (
+  <>
+    <circle cx="10" cy="17.5" r="2.5" transform="rotate(-90 10 17.5)" />
+    <circle cx="10" cy="10" r="2.5" transform="rotate(-90 10 10)" />
+    <circle cx="10" cy="2.5" r="2.5" transform="rotate(-90 10 2.5)" />
+  </>
+);
 
 export default MoreVerticalIcon;
