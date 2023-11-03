f49e25f040dbbc1af0f8f70f5d9d3206f951b8d7
Tye Stanley <tyestanley090@gmail.com>
2023-11-02 18:30:33 -0700

    feat: create content into the popover

--- components/navbar/UserButton.tsx
+++ components/navbar/UserButton.tsx
@@ -6,6 +6,8 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import Theme from "@/components/navbar/Theme";
+import FillIcon from "@/components/icons/fill-icons";
+import Link from "next/link";
 
 interface UserButtonProps {
   userImg: string | undefined;
@@ -23,7 +25,25 @@ const UserButton = async ({ userImg }: UserButtonProps) => {
           className="m-[2px] rounded-[0.375rem] bg-yellow-30"
         />
       </PopoverTrigger>
-      <PopoverContent className="relative right-[20px] top-[15px] h-[187px] w-[182px] bg-[url('/USERBUTTON_POPOVER_LIGHT.svg')] bg-center dark:bg-[url('/USERBUTTON_POPOVER_DARK.svg')]">
+      <PopoverContent className="relative right-[20px] top-[15px] flex h-[187px] w-[182px] flex-col justify-center gap-5 bg-[url('/USERBUTTON_POPOVER_LIGHT.svg')] bg-center dark:bg-[url('/USERBUTTON_POPOVER_DARK.svg')]">
+        <Link
+          href="/profile"
+          className="flex items-center gap-3.5 text-[1rem] font-semibold leading-[1.5rem] text-sc-2 dark:text-light-2"
+        >
+          <FillIcon.Profile className="fill-sc-2 dark:fill-light-2" />
+          Profile
+        </Link>
+
+        <Link
+          href="/settings"
+          className="flex items-center gap-3.5 text-[1rem] font-semibold leading-[1.5rem] text-sc-2 dark:text-light-2"
+        >
+          <FillIcon.Settings className="fill-sc-2 dark:fill-light-2" />
+          Settings
+        </Link>
+
+        <div className="h-[1px] w-full bg-light-2 dark:bg-sc-3" />
+
         <div className="flex items-center justify-between">
           <p className="text-[1rem] font-semibold leading-[1.5rem] text-sc-2 dark:text-light-2">
             Interface
