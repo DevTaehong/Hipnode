87bce08eec02268331c24cfcf41ba42460bd70db
DevTaehong <minth1123@icloud.com>
2023-11-07 14:15:52 -0400

    feat: Create a group about component

---
+++ app/(root)/group/[id]/page.tsx
@@ -0,0 +1,11 @@
+import GroupAbout from "@/components/group-detail-page/GroupAbout";
+
+const GroupDetailPage = () => {
+  return (
+    <div className="bg-light-2_dark-2 flex h-screen flex-col p-5">
+      <GroupAbout />
+    </div>
+  );
+};
+
+export default GroupDetailPage;
