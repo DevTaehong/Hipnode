d5143655eb72e5b061add6eb018489c8766ad741
DevTaehong <minth1123@icloud.com>
2023-11-10 22:50:09 -0400

    feat: Add ActiveMembers component to GroupDetailPage

--- app/(root)/group/[id]/page.tsx
+++ app/(root)/group/[id]/page.tsx
@@ -1,3 +1,4 @@
+import ActiveMembers from "@/components/group-detail-page/active-members/ActiveMembers";
 import GroupAbout from "@/components/group-detail-page/GroupAbout";
 import GroupAdmins from "@/components/group-detail-page/GroupAdmins";
 import GroupCover from "@/components/group-detail-page/GroupCover";
@@ -8,10 +9,11 @@ const GroupDetailPage = async ({ params }: { params: { id: string } }) => {
   const group = await getGroupById({ groupId });
 
   return (
-    <div className="bg-light-2_dark-2 flex h-screen flex-col gap-5 p-5">
+    <div className="bg-light-2_dark-2 flex flex-col gap-5 p-5">
       <GroupCover group={group} />
       <GroupAbout />
       <GroupAdmins />
+      <ActiveMembers members={group?.members ?? []} />
     </div>
   );
 };
