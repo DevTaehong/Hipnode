735adc4dab5dfd84c0cdfd435c697de3887febb9
DevTaehong <minth1123@icloud.com>
2023-11-11 00:34:14 -0400

    feat: Add RecentMedia component to GroupDetailPage

--- app/(root)/group/[id]/page.tsx
+++ app/(root)/group/[id]/page.tsx
@@ -1,6 +1,7 @@
 import GroupAbout from "@/components/group-detail-page/GroupAbout";
 import GroupAdmins from "@/components/group-detail-page/GroupAdmins";
 import GroupCover from "@/components/group-detail-page/GroupCover";
+import RecentMedia from "@/components/group-detail-page/RecentMedia";
 import { getGroupById } from "@/lib/actions/group.actions";
 
 const GroupDetailPage = async ({ params }: { params: { id: string } }) => {
@@ -8,10 +9,12 @@ const GroupDetailPage = async ({ params }: { params: { id: string } }) => {
   const group = await getGroupById({ groupId });
 
   return (
-    <div className="bg-light-2_dark-2 flex h-screen flex-col gap-5 p-5">
+    <div className="bg-light-2_dark-2 flex flex-col gap-5 p-5">
       <GroupCover group={group} />
       <GroupAbout />
       <GroupAdmins />
+
+      <RecentMedia media={group?.posts ?? []} />
     </div>
   );
 };
