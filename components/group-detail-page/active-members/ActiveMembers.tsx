3ed79421a85469e9daa9886c35c77e3a90c9d8f9
DevTaehong <minth1123@icloud.com>
2023-11-14 00:40:48 -0400

    refactor: Remove unnecessary width property from ActiveMembers component

--- components/group-detail-page/active-members/ActiveMembers.tsx
+++ components/group-detail-page/active-members/ActiveMembers.tsx
@@ -5,7 +5,7 @@ import TotalMembers from "@/components/group-detail-page/active-members/TotalMem
 
 const ActiveMembers = ({ members }: { members: User[] }) => {
   return (
-    <div className="bg-light_dark-3 flex h-[11.5rem] w-[20.9375rem] flex-col gap-5 rounded-2xl p-5">
+    <div className="bg-light_dark-3 flex h-[11.5rem] flex-col gap-5 rounded-2xl p-5">
       <p className="semibold-16 text-sc-2 dark:text-light-2">Active Members</p>
       <div className="flex flex-row flex-wrap items-start gap-x-[1.3125rem] gap-y-5">
         {members.slice(0, 10).map((member, i) =>
