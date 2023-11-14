0931a1644ba56fb404cdc0c01fde6c594060d8ed
DevTaehong <minth1123@icloud.com>
2023-11-14 01:03:55 -0400

    refactor: Refactor GroupDetailPost component with new header and footer components

--- components/group-detail-page/group-detail-post/GroupDetailPost.tsx
+++ components/group-detail-page/group-detail-post/GroupDetailPost.tsx
@@ -4,12 +4,10 @@ import Image from "next/image";
 import { Post } from "@prisma/client";
 import Link from "next/link";
 
-import Tag from "@/components/profile/Tag";
-import StatsDescription from "@/components/profile/StatsDescription";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Heart from "@/components/icons/fill-icons/Heart";
 import { ExtendedPost } from "@/types/models";
 import { formatGroupDetailPostDate } from "@/utils";
+import GroupDetailPostHeader from "@/components/group-detail-page/group-detail-post/GroupDetailPostHeader";
+import GroupDetailPostFooter from "@/components/group-detail-page/group-detail-post/GroupDetailPostFooter";
 
 const GroupDetailPost = (post: Post) => {
   const { id, author, image, createdAt, heading } = post as ExtendedPost;
@@ -30,63 +28,8 @@ const GroupDetailPost = (post: Post) => {
           lg:h-14 lg:w-14 lg:border lg:border-contentCard lg:shadow-contentCard xl:h-[9.75rem] xl:w-[9.75rem]"
       />
       <div className="flex grow flex-col justify-between gap-5">
-        <div className="flex flex-row justify-between">
-          <div className="flex flex-col gap-2.5">
-            <h3
-              className={`semibold-12 lg:semibold-12 xl:semibold-18 md:semibold-18 line-clamp-3 text-sc-2 dark:text-light-2 lg:line-clamp-2`}
-            >
-              {heading}
-            </h3>
-            <div className="flex flex-row gap-2.5">
-              <Tag text={"Bitcoin"} />
-              <Tag text={"Bitcoin"} />
-              <Tag text={"Bitcoin"} />
-            </div>
-          </div>
-          <Avatar className="flex h-[1.875rem] w-[1.875rem] shrink-0 md:h-10 md:w-10 xl:hidden">
-            <AvatarImage
-              src={author.picture}
-              alt={`${author.username}'s avatar`}
-            />
-            <AvatarFallback>{author.username.charAt(0)}</AvatarFallback>
-          </Avatar>
-          <Heart className={"hidden xl:block"} />
-        </div>
-        <div className="flex flex-row gap-[1.875rem] lg:items-center lg:justify-between lg:gap-10">
-          <div className="hidden xl:flex xl:flex-row xl:gap-2.5">
-            <Avatar className="h-10 w-10 shrink-0">
-              <AvatarImage
-                src={author.picture}
-                alt={`${author.username}'s avatar`}
-              />
-              <AvatarFallback>{author.username.charAt(0)}</AvatarFallback>
-            </Avatar>
-            <div className="flex flex-col">
-              <div className="flex flex-row items-center gap-1">
-                <p className="semibold-14 text-sc-2">{author.username}</p>
-                <p className="text-sc-5">{`•`}</p>
-              </div>
-              <p className="regular-10 text-sc-3">{date}</p>
-            </div>
-          </div>
-          <div className="flex flex-row gap-[1.875rem] xl:items-center xl:gap-10">
-            <StatsDescription
-              className={`md:text-[0.875rem] md:leading-[1.375rem] lg:text-[0.5625rem] lg:leading-[0.875rem] xl:text-[0.875rem] xl:leading-[1.375rem]`}
-            >
-              {"651,324"} Views
-            </StatsDescription>
-            <StatsDescription
-              className={`md:text-[0.875rem] md:leading-[1.375rem] lg:text-[0.5625rem] lg:leading-[0.875rem] xl:text-[0.875rem] xl:leading-[1.375rem]`}
-            >
-              {"36,6545"} Likes
-            </StatsDescription>
-            <StatsDescription
-              className={`md:text-[0.875rem] md:leading-[1.375rem] lg:text-[0.5625rem] lg:leading-[0.875rem] xl:text-[0.875rem] xl:leading-[1.375rem]`}
-            >
-              {56} Comments
-            </StatsDescription>
-          </div>
-        </div>
+        <GroupDetailPostHeader heading={heading} author={author} />
+        <GroupDetailPostFooter author={author} date={date} />
       </div>
     </Link>
   );
