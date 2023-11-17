7c646d81ebe8254b40acdc77782f06fe23b779bd
DevTaehong <minth1123@icloud.com>
2023-11-16 22:57:07 -0400

    refactor: Add FetchGroupDetailPosts component

---
+++ components/group-detail-page/FetchGroupDetailPosts.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Post } from "@prisma/client";
+import { useSearchParams } from "next/navigation";
+
+import InfiniteScroll from "@/components/InfiniteScroll";
+import GroupDetailPost from "./group-detail-post/GroupDetailPost";
+
+const FetchGroupDetailPosts = ({
+  initialNewPost,
+  initialPopularPost,
+  fetchNewPost,
+  fetchPopularPost,
+  groupId,
+}: {
+  initialNewPost: Post[];
+  initialPopularPost: Post[];
+  fetchNewPost: (myCursorId: number, groupId?: number) => Promise<Post[]>;
+  fetchPopularPost: (myCursorId: number, groupId?: number) => Promise<Post[]>;
+  groupId: number;
+}) => {
+  const searchParams = useSearchParams();
+  const explore = searchParams.get("posts");
+
+  const fetchData = explore === "Popular" ? fetchPopularPost : fetchNewPost;
+  const initialData =
+    explore === "Popular" ? initialPopularPost : initialNewPost;
+
+  return (
+    <InfiniteScroll
+      initialData={initialData}
+      fetchData={fetchData}
+      renderItem={GroupDetailPost}
+      className="flex flex-col gap-5"
+      groupId={groupId}
+    />
+  );
+};
+
+export default FetchGroupDetailPosts;
