09e6bf1f36c387e6cf38e689fd9cd904fe6b2fdb
DevTaehong <minth1123@icloud.com>
2023-11-20 16:43:22 -0400

    refactor: Fix key prop in Admins component

--- components/group-detail-page/Admins.tsx
+++ components/group-detail-page/Admins.tsx
@@ -13,21 +13,21 @@ const Admins = ({
       {admins.map((admin) => (
         <div
           className="flex flex-row items-center justify-between"
-          key={admin.username}
+          key={admin.id}
         >
           <Link
             href={`/profile/${admin.id}`}
-            className="flex items-center gap-2.5 hover:opacity-80 hover:transition-opacity"
+            className="line-clamp-1 flex items-center gap-2.5 hover:opacity-80 hover:transition-opacity"
           >
             <Avatar className="h-[1.875rem] w-[1.875rem]">
               <AvatarImage src={admin.picture} alt={admin.username} />
               <AvatarFallback>{admin.username.charAt(0)}</AvatarFallback>
             </Avatar>
-            <p className="semibold-14 font-feature text-sc-2 dark:text-light-2">
+            <p className="semibold-14 font-feature truncate text-sc-2 dark:text-light-2">
               {admin.username}
             </p>
           </Link>
-          <div className="flex h-[1.875rem] w-[1.875rem] items-center justify-center rounded-full bg-blue-10">
+          <div className="flex h-[1.875rem] w-[1.875rem] shrink-0 items-center justify-center rounded-full bg-blue-10">
             {/* // TODO - add follow functionality */}
             <OutlineIcon.Follow className="fill-blue-10" />
           </div>
