b1f382a7052b1b959857dca2f2e3531b8879ba4b
AlexDjangoX <alexmonk17@gmail.com>
2023-11-20 17:34:29 +0100

    removed s

--- app/(root)/posts/post/[id]/page.tsx
+++ app/(root)/posts/post/[id]/page.tsx
@@ -82,7 +82,6 @@ const PostPage = async ({ params }: { params: { id: number } }) => {
               <DevelopmentInformation devInfo={devInfo} />
             </div>
           </aside>
-          s
         </div>
       </div>
     </main>
