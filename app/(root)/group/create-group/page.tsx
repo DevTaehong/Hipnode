3b9b0d41e8a0061806238c0788d472781ae320e9
DevTaehong <minth1123@icloud.com>
2023-11-23 03:20:15 -0400

    refactor: Refactor CreateGroupPage component and add user-related functionality

--- app/(root)/group/create-group/page.tsx
+++ app/(root)/group/create-group/page.tsx
@@ -1,10 +1,29 @@
-import GroupForm from "@/components/group-form/GroupForm";
+import { auth } from "@clerk/nextjs";
 
-const CreateGroupPage = () => {
+import GroupForm from "@/components/group-form/GroupForm";
+import {
+  getAllUsers,
+  getUserByClerkId,
+  getUserById,
+} from "@/lib/actions/user.actions";
+
+const CreateGroupPage = async () => {
+  const users = await getAllUsers();
+  const { userId: clerkId } = await auth();
+
+  let user;
+
+  if (clerkId) user = await getUserByClerkId(clerkId);
+
+  // NOTE - To add a user to a group as an admin, member, and creator
+  const currentUser = await getUserById(user?.id ?? -1);
+
+  if (!currentUser) throw new Error("User not found");
+
   return (
     <div className="bg-light-2_dark-2">
-      <div className="h-screen p-5 sm:pt-[1.875rem]">
-        <GroupForm />
+      <div className="p-5 sm:pt-[1.875rem]">
+        <GroupForm users={users} currentUser={currentUser} />
       </div>
     </div>
   );
