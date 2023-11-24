946a4e76204b7fd6d05193c0636de47bc45e7592
Tye Stanley <tyestanley090@gmail.com>
2023-11-24 10:11:31 -0800

    fix: spacing on the user popover

--- components/navbar/UserButton.tsx
+++ components/navbar/UserButton.tsx
@@ -27,17 +27,19 @@ const UserButton = () => {
         />
       </PopoverTrigger>
       <PopoverContent className="relative right-[20px] top-[15px] flex h-[187px] w-[182px] flex-col justify-center gap-5 bg-[url('/USERBUTTON_POPOVER_LIGHT.svg')] bg-center dark:bg-[url('/USERBUTTON_POPOVER_DARK.svg')]">
-        <UserButtonLink link="/profile" text="Profile" />
+        <div className="relative top-[5px] flex flex-col gap-5">
+          <UserButtonLink link="/profile" text="Profile" />
 
-        <UserButtonLink link="/settings" text="Settings" />
+          <UserButtonLink link="/settings" text="Settings" />
 
-        <div className="h-[1px] w-full bg-light-2 dark:bg-sc-3" />
+          <div className="h-[1px] w-full bg-light-2 dark:bg-sc-3" />
 
-        <div className="flex items-center justify-between">
-          <p className="text-[1rem] font-semibold leading-[1.5rem] text-sc-2 dark:text-light-2">
-            Interface
-          </p>
-          <Theme />
+          <div className="flex items-center justify-between">
+            <p className="text-[1rem] font-semibold leading-[1.5rem] text-sc-2 dark:text-light-2">
+              Interface
+            </p>
+            <Theme />
+          </div>
         </div>
       </PopoverContent>
     </Popover>
