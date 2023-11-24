d8135d1aa214ed3367407ca1c38c1eec472b5e92
DevTaehong <minth1123@icloud.com>
2023-11-24 14:28:36 -0400

    refactor: Refactor user retrieval in CreateGroupPage component

--- app/(root)/group/create-group/page.tsx
+++ app/(root)/group/create-group/page.tsx
@@ -2,29 +2,23 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import GroupForm from "@/components/group-form/GroupForm";
-import {
-  getAllUsers,
-  getUserByClerkId,
-  getUserById,
-} from "@/lib/actions/user.actions";
+import { getAllUsers, getUserByClerkId } from "@/lib/actions/user.actions";
 
 const CreateGroupPage = async () => {
+  // NOTE - Get all users to be used in the AddAdminsOrMembers component
   const users = await getAllUsers();
 
   const { userId: clerkId } = auth();
   if (!clerkId) redirect("/sign-in");
 
-  const user = await getUserByClerkId(clerkId);
-  if (!user) redirect("/sign-in");
-
   // NOTE - To add a user to a group as an admin, member, and creator, get the user data
-  const currentUser = await getUserById(user.id);
-  if (!currentUser) redirect("/sign-in");
+  const user = await getUserByClerkId(clerkId, false);
+  if (!user) redirect("/sign-in");
 
   return (
     <div className="bg-light-2_dark-2">
       <div className="p-5 sm:pt-[1.875rem]">
-        <GroupForm users={users} currentUser={currentUser} />
+        <GroupForm users={users} currentUser={user} />
       </div>
     </div>
   );
