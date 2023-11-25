3a4af0da93823e37958521f352b4cf3138126ce1
Tye Stanley <tyestanley090@gmail.com>
2023-11-24 16:09:08 -0800

    feat: added username next to the user image on larger screens

--- components/navbar/UserButton.tsx
+++ components/navbar/UserButton.tsx
@@ -15,16 +15,31 @@ import UserButtonLink from "./UserButtonLink";
 const UserButton = () => {
   const { user } = useUser();
   const userImg = user?.imageUrl;
+  console.log(user);
   return (
     <Popover>
-      <PopoverTrigger className="shrink-0 rounded-[0.5rem] border-[1px] border-yellow">
-        <Image
-          src={userImg ?? "/christopher.png"}
-          alt="User Image"
-          width={30}
-          height={30}
-          className="m-[2px] rounded-[0.375rem] bg-yellow-30"
-        />
+      <PopoverTrigger className="flex items-center justify-center gap-4">
+        <div className="shrink-0 rounded-[0.5rem] border-[1px] border-yellow">
+          <Image
+            src={userImg ?? "/christopher.png"}
+            alt="User Image"
+            width={30}
+            height={30}
+            className="m-[2px] rounded-[0.375rem] bg-yellow-30"
+          />
+        </div>
+
+        <p className="hidden items-center justify-center gap-2.5 text-[1rem] font-bold leading-[1.5rem] text-sc-1 dark:text-light-2 xl:flex">
+          {user?.username}
+
+          <Image
+            src="/navbar/triangle_down.svg"
+            alt="User Image"
+            width={20}
+            height={20}
+            className="shrink-0"
+          />
+        </p>
       </PopoverTrigger>
       <PopoverContent className="relative right-[20px] top-[15px] flex h-[187px] w-[182px] flex-col justify-center gap-5 bg-[url('/USERBUTTON_POPOVER_LIGHT.svg')] bg-center dark:bg-[url('/USERBUTTON_POPOVER_DARK.svg')]">
         <div className="relative top-[5px] flex flex-col gap-5">
