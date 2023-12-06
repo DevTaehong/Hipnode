b2f50bc083fd508a5c6be47bd85dcff6d66a7be4
DevTaehong <minth1123@icloud.com>
2023-12-06 18:37:13 -0400

    refactor: Refactor NotificationButton component

--- components/navbar/NotificationButton.tsx
+++ components/navbar/NotificationButton.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
   Popover,
   PopoverContent,
@@ -11,20 +9,51 @@ import { dummyNotifications } from "@/constants";
 
 const NotificationButton = () => {
   return (
-    <Popover>
-      <PopoverTrigger>
-        <div className="cursor-pointer rounded-lg bg-light-2 p-2 dark:bg-dark-4">
-          <FillIcon.Notification
-            className="fill-sc-4 dark:fill-sc-6"
-            notification
-          />
-        </div>
-      </PopoverTrigger>
-      <PopoverContent>
-        <NotificationPopover notifications={dummyNotifications} />
-      </PopoverContent>
-    </Popover>
+    <>
+      {/* // NOTE - Because of different offsets on mobile and desktop, we need to render two different */}
+      <NotificationPopoverButton
+        className="hidden xl:block"
+        sideOffset={11}
+        alignOffset={-189}
+      />
+      <NotificationPopoverButton
+        className="block xl:hidden"
+        sideOffset={15}
+        alignOffset={-72}
+      />
+    </>
   );
 };
 
 export default NotificationButton;
+
+const NotificationPopoverButton = ({
+  className,
+  sideOffset,
+  alignOffset,
+}: {
+  className: string;
+  sideOffset: number;
+  alignOffset: number;
+}) => (
+  <Popover>
+    <PopoverTrigger asChild className={className}>
+      <div className="cursor-pointer xl:rounded-lg xl:bg-light-2 xl:p-2 dark:xl:bg-dark-4">
+        <FillIcon.Notification
+          className="fill-sc-4 dark:fill-sc-6"
+          notification
+        />
+      </div>
+    </PopoverTrigger>
+
+    <PopoverContent
+      sideOffset={sideOffset}
+      align="end"
+      alignOffset={alignOffset}
+      className="w-full"
+      avoidCollisions={false}
+    >
+      <NotificationPopover notifications={dummyNotifications} />
+    </PopoverContent>
+  </Popover>
+);
