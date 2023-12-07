6c742fff9cae09643777da2b1093b7ab6ca65aa4
DevTaehong <minth1123@icloud.com>
2023-12-07 14:47:40 -0400

    refactor: Refactor NotificationButton component

--- components/navbar/NotificationButton.tsx
+++ components/navbar/NotificationButton.tsx
@@ -1,22 +1,15 @@
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import FillIcon from "../icons/fill-icons";
-import NotificationPopover from "../notificationPopover/NotificationPopover";
-import { dummyNotifications } from "@/constants";
+import NotificationPopover from "./NotificationPopover";
 
 const NotificationButton = () => {
   return (
     <>
       {/* // NOTE - Because of different offsets on mobile and desktop, we need to render two different */}
-      <NotificationPopoverButton
+      <NotificationPopover
         className="hidden xl:block"
         sideOffset={11}
         alignOffset={-189}
       />
-      <NotificationPopoverButton
+      <NotificationPopover
         className="block xl:hidden"
         sideOffset={15}
         alignOffset={-72}
@@ -26,34 +19,3 @@ const NotificationButton = () => {
 };
 
 export default NotificationButton;
-
-const NotificationPopoverButton = ({
-  className,
-  sideOffset,
-  alignOffset,
-}: {
-  className: string;
-  sideOffset: number;
-  alignOffset: number;
-}) => (
-  <Popover>
-    <PopoverTrigger asChild className={className}>
-      <div className="cursor-pointer xl:rounded-lg xl:bg-light-2 xl:p-2 dark:xl:bg-dark-4">
-        <FillIcon.Notification
-          className="fill-sc-4 dark:fill-sc-6"
-          notification
-        />
-      </div>
-    </PopoverTrigger>
-
-    <PopoverContent
-      sideOffset={sideOffset}
-      align="end"
-      alignOffset={alignOffset}
-      className="w-full"
-      avoidCollisions={false}
-    >
-      <NotificationPopover notifications={dummyNotifications} />
-    </PopoverContent>
-  </Popover>
-);
