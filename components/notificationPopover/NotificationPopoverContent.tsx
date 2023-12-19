4ec467612140a474883b13a20208645dc72d6c11
DevTaehong <minth1123@icloud.com>
2023-12-18 23:32:28 -0400

    refactor: Update notification message to include selected tab

--- components/notificationPopover/NotificationPopoverContent.tsx
+++ components/notificationPopover/NotificationPopoverContent.tsx
@@ -83,7 +83,13 @@ const NotificationPopoverContent = ({
               ))
             ) : (
               <p className="mt-3 text-center text-[1rem] font-bold leading-[1.5rem] text-sc-2 dark:text-light-2 xl:mt-5 xl:text-[1.625rem] xl:leading-[2.375rem]">
-                No Notifications
+                No Notifications{" "}
+                {selectedTab !== "all notification" && selectedTab
+                  ? "For " +
+                    selectedTab.charAt(0).toUpperCase() +
+                    selectedTab.slice(1) +
+                    "s"
+                  : ""}
               </p>
             )}
             <Link
