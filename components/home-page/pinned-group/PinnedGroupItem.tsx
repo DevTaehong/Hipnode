f7e0c79da20fa5b549e565dc9c45b863d02b0b1b
AlexDjangoX <alexmonk17@gmail.com>
2023-12-23 21:47:10 +0100

    wired up pinned groups

--- components/home-page/pinned-group/PinnedGroupItem.tsx
+++ components/home-page/pinned-group/PinnedGroupItem.tsx
@@ -1,25 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 
-import { christopher } from "@/public/assets";
 import { PinnedGroupItemProps } from "@/types/homepage";
 
-const PinnedGroupItem = ({
-  group: { name, description },
-}: PinnedGroupItemProps) => (
-  <li className="flex items-center gap-2.5">
-    <Image
-      src={christopher}
-      alt={`Logo of group ${name} in list of pinned groups`}
-      height={32}
-      width={32}
-      className="rounded"
-    />
+const PinnedGroupItem = ({ group }: PinnedGroupItemProps) => {
+  const { name, description, coverImage, id } = group;
+  return (
+    <Link href={`/group/${id}`}>
+      <li className="flex cursor-pointer items-center gap-2.5 hover:translate-x-1 hover:scale-[101%]">
+        {coverImage && (
+          <Image
+            src={coverImage}
+            alt={`Logo of group ${name} in list of pinned groups`}
+            height={32}
+            width={32}
+            className="rounded"
+          />
+        )}
 
-    <article className="flex flex-col">
-      <h3 className="semibold-12 text-sc-4_light-2">{name}</h3>
-      <p className="base-9 text-sc-4 dark:text-sc-3">{description}</p>
-    </article>
-  </li>
-);
+        <article className="flex flex-col">
+          <h3 className="semibold-12 text-sc-4_light-2">{name}</h3>
+          <p className="base-9 line-clamp-1 text-sc-4 dark:text-sc-3">
+            {description}
+          </p>
+        </article>
+      </li>
+    </Link>
+  );
+};
 
 export default PinnedGroupItem;
