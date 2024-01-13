ef54bdb7c0ce6ecd86ecf9dcdca8da3d300a92bb
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-01-13 03:44:56 +0200

    further simplified set online users

--- components/live-chat/ChatPageLink.tsx
+++ components/live-chat/ChatPageLink.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useChannel, usePresence } from "ably/react";
 
 import useChatStore from "@/app/chatStore";
@@ -9,19 +9,15 @@ import { ChatPageLinkProps } from "@/types/searchbar.index";
 
 const ChatPageLink = ({ userInfo, userChatrooms }: ChatPageLinkProps) => {
   const { setOnlineUsers } = useChatStore();
-  const [onlineUsersPresent, setOnlineUsersPresent] = useState<number[] | []>(
-    []
-  );
 
   const { presenceData } = usePresence("online-user");
+  const { channel } = useChannel("online-user");
 
   useEffect(() => {
-    setOnlineUsersPresent(
+    setOnlineUsers(
       presenceData.map((presenceItem) => presenceItem.data?.userId)
     );
   }, [presenceData]);
-
-  const { channel } = useChannel("online-user");
 
   useEffect(() => {
     channel.presence.enter({ userId: userInfo.id });
@@ -30,10 +26,6 @@ const ChatPageLink = ({ userInfo, userChatrooms }: ChatPageLinkProps) => {
       channel.presence.leave({ userId: userInfo.id });
     };
   }, [channel, userInfo.id]);
-
-  useEffect(() => {
-    setOnlineUsers(onlineUsersPresent);
-  }, [onlineUsersPresent]);
 
   return <NavBarChatList userChatrooms={userChatrooms} />;
 };
