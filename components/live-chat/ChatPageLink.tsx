abefb77edcc94dc80f3aee6de1130aff8d526738
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-01-13 02:24:39 +0200

    working much better

--- components/live-chat/ChatPageLink.tsx
+++ components/live-chat/ChatPageLink.tsx
@@ -1,55 +1,57 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useChannel } from "ably/react";
+import { useChannel, usePresence } from "ably/react";
 
 import useChatStore from "@/app/chatStore";
 import NavBarChatList from "./NavBarChatList";
 import { ChatPageLinkProps } from "@/types/searchbar.index";
 
 const ChatPageLink = ({ userInfo, userChatrooms }: ChatPageLinkProps) => {
-  const { setOnlineUsers } = useChatStore();
+  const { setOnlineUsers, onlineUsers } = useChatStore();
   const [onlineUsersPresent, setOnlineUsersPresent] = useState<number[] | []>(
     []
   );
 
-  const { channel } = useChannel("online-user", (message: any) => {
-    const userOnlineInfo = message.data.data;
+  const { presenceData } = usePresence("online-user");
 
-    if (userOnlineInfo.status === "online") {
-      setOnlineUsersPresent((prevOnlineUsers: number[]) => {
-        if (!prevOnlineUsers.includes(userOnlineInfo.userId)) {
-          return [...prevOnlineUsers, userOnlineInfo.userId];
-        } else {
-          return prevOnlineUsers;
-        }
-      });
-    } else {
-      setOnlineUsersPresent((prevOnlineUsers) => {
-        return prevOnlineUsers.filter(
-          (userId) => userId !== userOnlineInfo.userId
-        );
-      });
-    }
+  console.log(presenceData);
+  console.log(onlineUsers);
+
+  useEffect(() => {
+    setOnlineUsersPresent(
+      presenceData.map((presenceItem) => presenceItem.data.userId)
+    );
+  }, [presenceData]);
+
+  const { channel } = useChannel("online-user", (message: any) => {
+    console.log(message);
+    // const userOnlineInfo = message.data.data;
+
+    // if (userOnlineInfo.status === "online") {
+    //   setOnlineUsersPresent((prevOnlineUsers: number[]) => {
+    //     if (!prevOnlineUsers.includes(userOnlineInfo.userId)) {
+    //       return [...prevOnlineUsers, userOnlineInfo.userId];
+    //     } else {
+    //       return prevOnlineUsers;
+    //     }
+    //   });
+    // } else {
+    //   setOnlineUsersPresent((prevOnlineUsers) => {
+    //     return prevOnlineUsers.filter(
+    //       (userId) => userId !== userOnlineInfo.userId
+    //     );
+    //   });
+    // }
   });
 
   useEffect(() => {
-    channel.publish("online-user", {
-      data: {
-        userId: userInfo.id,
-        status: "online",
-      },
-    });
+    channel.presence.enter({ userId: userInfo.id, status: "online" });
 
     return () => {
-      channel.publish("online-user", {
-        data: {
-          userId: userInfo.id,
-          status: "offline",
-        },
-      });
+      channel.presence.leave({ userId: userInfo.id, status: "offline" });
     };
-  }, []);
+  }, [channel, userInfo.id]);
 
   useEffect(() => {
     setOnlineUsers(onlineUsersPresent);
