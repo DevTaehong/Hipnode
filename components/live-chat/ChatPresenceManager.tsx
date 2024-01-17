b863fd81d253b84bd38bd7a5bf7af78feaa5affa
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-01-17 14:32:32 +0200

    fixed PR issues

--- components/live-chat/ChatPresenceManager.tsx
+++ components/live-chat/ChatPresenceManager.tsx
@@ -1,27 +1,40 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useChannel, usePresence } from "ably/react";
 
 import useChatStore from "@/app/chatStore";
 import { BaseUserInfo } from "@/types/profile.index";
 
 const ChatPresenceManager = ({ userInfo }: { userInfo: BaseUserInfo }) => {
-  const { setOnlineUsers } = useChatStore();
+  const { setOnlineUsers, onlineUsers } = useChatStore();
   const { presenceData } = usePresence("online-user");
   const { channel } = useChannel("online-user");
 
-  useEffect(() => {
-    setOnlineUsers(
-      presenceData.map((presenceItem) => presenceItem.data?.userId)
-    );
-  }, [presenceData]);
+  const onlineUserIds = useMemo(
+    () => presenceData.map((presenceItem) => presenceItem.data?.userId),
+    [presenceData]
+  );
+
+  console.log(onlineUsers);
 
   useEffect(() => {
-    channel.presence.enter({ userId: userInfo.id });
+    if (onlineUserIds) setOnlineUsers(onlineUserIds);
+  }, [onlineUserIds]);
+
+  useEffect(() => {
+    try {
+      channel.presence.enter({ userId: userInfo.id });
+    } catch (error) {
+      console.error("Error entering presence", error);
+    }
 
     return () => {
-      channel.presence.leave({ userId: userInfo.id });
+      try {
+        channel.presence.leave({ userId: userInfo.id });
+      } catch (error) {
+        console.error("Error leaving presence", error);
+      }
     };
   }, [channel]);
 
