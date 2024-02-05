680323f41a307ce7e1b080dfbbcfd8e579d2d423
DevTaehong <minth1123@icloud.com>
2024-02-05 16:09:20 -0400

    refactor: refactor conditional rendering in ProfileResult component

--- components/profile/ProfileResult.tsx
+++ components/profile/ProfileResult.tsx
@@ -25,43 +25,54 @@ const ProfileResult = ({
     </div>;
 
   if (searchParam === "posts" && Array.isArray(result) && result.length !== 0)
-    <PostCardList posts={result as ExtendedPrismaPost[]} authorId={authorId} />;
+    return (
+      <PostCardList
+        posts={result as ExtendedPrismaPost[]}
+        authorId={authorId}
+      />
+    );
 
   if (searchParam === "meetups" && "data" in result && result.data.length !== 0)
-    <MeetupsList
-      data={result as ProfileMeetupResponse}
-      authorId={paramId}
-      resultType={searchParam}
-    />;
+    return (
+      <MeetupsList
+        data={result as ProfileMeetupResponse}
+        authorId={paramId}
+        resultType={searchParam}
+      />
+    );
 
   if (
     searchParam === "podcasts" &&
     "data" in result &&
     result.data.length !== 0
   )
-    <PodcastsList
-      data={result as ProfilePodcastResponse}
-      authorId={paramId}
-      resultType={searchParam}
-    />;
+    return (
+      <PodcastsList
+        data={result as ProfilePodcastResponse}
+        authorId={paramId}
+        resultType={searchParam}
+      />
+    );
 
   if (
     searchParam === "interviews" &&
     "data" in result &&
     result.data.length !== 0
   )
-    <div className="relative flex w-full flex-1 overflow-auto">
-      <InterviewsList
-        data={result as ProfileInterviewsResponse}
-        authorId={paramId}
-        resultType={searchParam}
-      />
-    </div>;
+    return (
+      <div className="relative flex w-full flex-1 overflow-auto">
+        <InterviewsList
+          data={result as ProfileInterviewsResponse}
+          authorId={paramId}
+          resultType={searchParam}
+        />
+      </div>
+    );
 
   if (searchParam === "history" && "data" in result && result.data.length !== 0)
-    <HistoryList data={result as ProfileHistoryResponse} authorId={paramId} />;
-
-  return null;
+    return (
+      <HistoryList data={result as ProfileHistoryResponse} authorId={paramId} />
+    );
 };
 
 export default ProfileResult;
