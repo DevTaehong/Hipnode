9d606ff3554d7bbc4f4641046477a017a0bf044b
DevTaehong <minth1123@icloud.com>
2024-02-06 05:48:47 -0400

    refactor: add lucide-react icons and apply hover effect to action popover buttons

--- components/action-popover/ActionPopover.tsx
+++ components/action-popover/ActionPopover.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { useState } from "react";
+import { MoreHorizontal, MoreVertical } from "lucide-react";
+
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { MoreHorizontal, MoreVertical } from "lucide-react";
-
 import CommentIconButton from "../posts/comment/CommentIconButton";
 import OutlineIcon from "@/components/icons/outline-icons";
 
@@ -52,16 +52,16 @@ const ActionPopover = ({
       <PopoverTrigger onClick={togglePopover}>
         <CommentIconButton
           Icon={isComment ? MoreHorizontal : MoreVertical}
-          color="text-sc-3"
+          color={`${isComment ? "text-sc-3" : "text-sc-5"} hover-effect`}
         />
       </PopoverTrigger>
       <PopoverContent>
         <section
-          className={`${positionStyles} flex w-fit flex-col rounded-xl border border-solid border-sc-5 bg-light-2 p-[1.25rem] dark:border-dark-3 dark:bg-dark-4`}
+          className={`${positionStyles} flex w-fit flex-col rounded-xl border border-solid border-sc-5 bg-light-2 p-[1.25rem] dark:border-sc-3 dark:bg-dark-4`}
         >
           <div
             onClick={onEditClick}
-            className="mb-[0.625rem] flex cursor-pointer flex-row items-center justify-start"
+            className="hover-effect mb-[0.625rem] flex cursor-pointer flex-row items-center justify-start"
           >
             <div className="fill-light-2">
               <OutlineIcon.Edit />
@@ -73,16 +73,12 @@ const ActionPopover = ({
 
           <div
             onClick={handleDeleteClick}
-            className="flex cursor-pointer items-center justify-start"
+            className="hover-effect flex cursor-pointer items-center justify-start"
           >
             <div className="fill-light-2 dark:fill-dark-4">
               <OutlineIcon.Trash />
             </div>
-            <p
-              className={`pl-[0.625rem] text-[0.875rem] font-semibold leading-[1.375rem] ${
-                isConfirmingDelete ? "text-red" : "text-red-60"
-              }`}
-            >
+            <p className="pl-[0.625rem] text-[0.875rem] font-semibold leading-[1.375rem] text-red dark:text-red-70">
               {isConfirmingDelete ? "Confirm Delete" : `Delete ${label}`}
             </p>
           </div>
@@ -90,7 +86,7 @@ const ActionPopover = ({
           {isConfirmingDelete && (
             <div
               onClick={handleCancelDelete}
-              className="flex cursor-pointer items-center justify-start"
+              className="hover-effect flex cursor-pointer items-center justify-start"
             >
               <p className="pl-[1.9rem] pt-1 text-[0.875rem] font-semibold leading-[1.375rem] text-blue dark:text-blue">
                 Cancel Delete
