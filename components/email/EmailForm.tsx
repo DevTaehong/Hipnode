777c0f88410ac74861792f102f8a7d884766c2b6
DevTaehong <minth1123@icloud.com>
2024-02-08 23:25:15 -0400

    refactor: update author name in EmailForm component

--- components/email/EmailForm.tsx
+++ components/email/EmailForm.tsx
@@ -9,7 +9,7 @@ import { sendEmail } from "@/lib/actions/email.actions";
 import { reportModalTags } from "@/constants";
 import { EmailFormProps } from "@/types/posts";
 
-const EmailForm = ({ currentUrl, setOpen, author }: EmailFormProps) => {
+const EmailForm = ({ currentUrl, setOpen, authorName }: EmailFormProps) => {
   const { toast } = useToast();
   const [selectedComplaintTag, setSelectedComplaintTag] = useState<string>("");
 
@@ -41,7 +41,7 @@ const EmailForm = ({ currentUrl, setOpen, author }: EmailFormProps) => {
     <section className="bg-light_dark-4 flex w-full max-w-[30rem] flex-col gap-[1.875rem] rounded-2xl p-5 md:p-[1.875rem]">
       <header>
         <h2 className="text-sc-2_light-2 semibold-18">
-          Why are you reporting this post by @{author}?
+          Why are you reporting this post by @{authorName}?
         </h2>
       </header>
       <div className="flex flex-wrap gap-5">
